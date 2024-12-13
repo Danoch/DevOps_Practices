4665ef90e45d7888ab8b4b0993b35ab7d87ff6cc
Daniel Martinez Velazquez <danoch@MacBook-Pro-de-Lizbeth.local>
2024-12-13 12:28:04 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevOpsDemoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
